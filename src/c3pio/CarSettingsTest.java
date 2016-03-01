691640823c27392d78c0b648fab54ff8c1f1e74d
kollstrom <e.kollstrom@gmail.com>
2016-03-01 18:29:15 +0100

    CarSettings and CarSettingsTest added

---
+++ src/c3pio/CarSettingsTest.java
@@ -0,0 +1,61 @@
+package c3pio;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by EmilOrvik on 01.03.2016.
+ */
+public class CarSettingsTest extends TestCase {
+
+    public void testSetSteeringWheelTilt() throws Exception {
+
+    }
+
+    public void testSetSteeringWheelDepth() throws Exception {
+
+    }
+
+    public void testSetRadioStation() throws Exception {
+
+    }
+
+    public void testSetWingMirrorLeftX() throws Exception {
+
+    }
+
+    public void testSetWingMirrorLeftY() throws Exception {
+
+    }
+
+    public void testSetWingMirrorRightX() throws Exception {
+
+    }
+
+    public void testSetWingMirrorRightY() throws Exception {
+
+    }
+
+    public void testSetSeatHeight() throws Exception {
+
+    }
+
+    public void testSetSeatDepth() throws Exception {
+
+    }
+
+    public void testSetSeatBackAngle() throws Exception {
+
+    }
+
+    public void testSetSeatHeadAngle() throws Exception {
+
+    }
+
+    public void testSetSeatBackDepth() throws Exception {
+
+    }
+
+    public void testSetTemperature() throws Exception {
+
+    }
+}
