e24367a5ab058c8e7086a486622603c25aa9dc20
emil <e.kollstrom@gmail.com>
2016-03-14 10:45:47 +0100

    Fra arduino til py(thon) til pi

---
+++ src/JavaRunCommand.java
@@ -0,0 +1,48 @@
+
+/**
+ * Write a description of class JavaRunCommand here.
+ * http://alvinalexander.com/java/edu/pj/pj010016
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+import java.io.*;
+ 
+public class JavaRunCommand {
+ 
+    public static void main(String args[]) {
+ 
+        String s = null;
+ 
+        try {
+             
+        // run the Unix "ps -ef" command
+            // using the Runtime exec method:
+            Process p = Runtime.getRuntime().exec("python arduinoReader.py");
+             
+            BufferedReader stdInput = new BufferedReader(new
+                 InputStreamReader(p.getInputStream()));
+ 
+            BufferedReader stdError = new BufferedReader(new
+                 InputStreamReader(p.getErrorStream()));
+ 
+            // read the output from the command
+            System.out.println("Here is the standard output of the command:\n");
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+             
+            // read any errors from the attempted command
+            System.out.println("Here is the standard error of the command (if any):\n");
+            while ((s = stdError.readLine()) != null) {
+                System.out.println(s);
+            }
+             
+            System.exit(0);
+        }
+        catch (IOException e) {
+            System.out.println("exception happened - here's what I know: ");
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+}
