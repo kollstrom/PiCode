c3d029c4da286cd893e85daf24ca316aa0aa2efd
emil <e.kollstrom@gmail.com>
2016-03-31 17:01:57 +0200

    La skripts til i src for enkelthetens skyld

--- pythonscripts/compile.py
+++
@@ -1,7 +0,0 @@
-import os
-
-os.system('export CLASSPATH="/home/pi/PiCode/jars/json-simple-1.1.1.jar:/home/pi/PiCode/src/"')
-os.system('javac /home/pi/PiCode/src/c3pio/CarSettings.java /home/pi/PiCode/src/c3pio/Controller.java '
-          '/home/pi/PiCode/src/c3pio/Simulator.java /home/pi/PiCode/src/c3pio/TCPClient.java'
-          '/home/pi/PiCode/src/c3pio/TCPServer.java')
-os.system('java -cp /home/pi/PiCode/src/c3pio/ Controller')
