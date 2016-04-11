008298968d7dd2728f96200c6420909e51355faf
Mikkel <mikkel.svagard@gmail.com>
2016-04-11 13:44:44 +0200

    La til kommentarer i controller og TCPServer

--- src/c3pio/TCPServer.java
+++ src/c3pio/TCPServer.java
@@ -23,37 +23,37 @@ class TCPServer implements Runnable{
     }
 
     public void run(){
+
+        /** This is a threaded method that keeps listening on socket 6789.
+         * When something is received the message gets parsed in the parseJSON method.
+         */
+
         try{
 
 
             ServerSocket welcomeSocket = new ServerSocket(6789);
             while (true){
-
                 Socket connectionSocket = welcomeSocket.accept();
                 this.inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
                 this.outToClient = new DataOutputStream(connectionSocket.getOutputStream());
-
                 String clientString = inFromClient.readLine();
                 JSONObject clientJSON = stringToJSON(clientString);
-
                 System.out.println(clientJSON);
-
                 parseJSON(clientJSON);
-
-
             }
-
-
         }
-        catch (FileNotFoundException e){
-            System.out.println("Someting wong");
-        }
+
         catch (Exception e){
-            System.out.println("Trouble with Json");
+            System.out.println("Couldn't convert client string to JSON");
         }
     }
 
     public void parseJSON(JSONObject payload) throws Exception{
+
+        /**
+        *The received object gets sent to the method that handles
+        * given request.
+         */
 
         String request = payload.get("request").toString();
         System.out.println("Client request: " + request);
@@ -68,44 +68,62 @@ class TCPServer implements Runnable{
                 this.requestSave(payload);
                 break;
 
-            case "login":
-                break;
-
             case "check":
                 this.requestCheck(payload);
                 break;
 
             default:
-                throw new Exception("Default thrown");
+                throw new Exception("No request found");
         }
     }
 
     public void requestSave(JSONObject payload){
+
+        /**
+         * Reads the current profile and sends it to client.
+         */
+
         JSONObject profile = controller.getCarSettingsAsJSON();
         try{
             writeResponse(stringToJSON(profile.toString()));
-        } catch (Exception e) {
-            System.out.println("Couldn't reply with profile");
-
+        }
+        catch (IOException e){
+            System.out.println("Couldn't write to client in requestSave");
+        }
+        catch (Exception e){
+            System.out.println("Couldn't convert to JSON in requestSave");
         }
     }
 
 
 
     public void requestExecute(JSONObject payload){
+
+        /**
+         * Executes the profile given by the client by uploading it into the carSettings.
+         */
+
         try{
             controller.setCarSettingsFromJSON(payload.get("profile").toString());
             JSONObject reply = new JSONObject();
             reply.put("message", "Profile executed");
             writeResponse(reply);
         }
+        catch (IOException e){
+            System.out.println("Couldn't write to client in requestExecute");
+        }
         catch (Exception e){
-            System.out.println("\"profile\" ekisterer ikke i pakken");
+            System.out.println("Wrong with JSON in requestExecute");
         }
 
     }
 
     public void requestCheck(JSONObject payload){
+
+        /**
+         * Stats reading from the alcometer and sends the reading to the client.
+         */
+
         double promilledouble = controller.alcoholMeasurement();
         String promille = String.valueOf(promilledouble);
         try{
@@ -113,8 +131,11 @@ class TCPServer implements Runnable{
             reply.put("message", promille);
             writeResponse(reply);
         }
+        catch (IOException e){
+            System.out.println("Couldn't write to client in requestCheck");
+        }
         catch(Exception e){
-            System.out.println("Wong with alcohol JSON" + "/n");
+            System.out.println("Couldnt convert to JSON in requestCheck");
 
         }
     }
