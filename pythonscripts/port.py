246e2a17a1aaaac11e9f2d22167d9921819d2a64
kollstrom <e.kollstrom@gmail.com>
2016-04-13 22:51:33 +0200

    raw-input-sjekk

--- pythonscripts/port.py
+++ pythonscripts/port.py
@@ -3,7 +3,7 @@ from difflib import Differ
 
 file = open('port.txt', 'w')
 before = os.popen('ls /dev/tty*').read()
-input('Connect the USB-cable from the Arduino to the Pi - then press ENTER')
+raw_input('Connect the USB-cable from the Arduino to the Pi - then press ENTER')
 after = os.popen('ls /dev/tty*').read()
 
 d = Differ()
