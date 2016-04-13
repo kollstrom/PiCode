2fcf4b0b821ce4b48a3f4cf2a7d0d7eea19a705f
kollstrom <e.kollstrom@gmail.com>
2016-04-13 23:32:40 +0200

    Compile

---
+++ pythonscripts/both.py
@@ -0,0 +1,25 @@
+import os
+from difflib import Differ
+
+# Find and set new port:
+
+file = open('port.txt', 'w')
+before = os.popen('ls /dev/tty*').read()
+raw_input('Connect the USB-cable from the Arduino to the Pi - then press ENTER')
+after = os.popen('ls /dev/tty*').read()
+
+d = Differ()
+diff = d.compare(before, after)
+plusdiff = "".join(diff)
+plus_start_index = plusdiff.find('+')
+plus_end_index = plusdiff.rfind('+') + 3
+new_port = plusdiff[plus_start_index:plus_end_index].replace('+', '').replace(' ', '').strip('\n')
+file.write(new_port)
+print(new_port)
+file.close()
+
+# Set CLASSPATH, compile and run:
+
+os.system('export CLASSPATH="/home/pi/PiCode/jars/json-simple-1.1.1.jar:/home/pi/PiCode/src"')
+os.system('javac /home/pi/PiCode/src/c3pio/*.java')
+os.system('java /home/pi/PiCode/src/c3pio/Controller')
--- pythonscripts/compile.py
+++ pythonscripts/compile.py
@@ -1,5 +1,5 @@
 import os
 
-os.system('export CLASSPATH="/home/pi/PiCode/jars/json-simple-1.1.1.jar:/home/pi/PiCode/src')
+os.system('export CLASSPATH="/home/pi/PiCode/jars/json-simple-1.1.1.jar:/home/pi/PiCode/src"')
 os.system('javac /home/pi/PiCode/src/c3pio/*.java')
 os.system('java /home/pi/PiCode/src/c3pio/Controller')
