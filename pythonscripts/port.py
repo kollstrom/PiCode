ae219fe100db5a2cedad286d4c186525dd64d865
kollstrom <e.kollstrom@gmail.com>
2016-04-13 23:17:59 +0200

    Get port

--- pythonscripts/port.py
+++ pythonscripts/port.py
@@ -8,4 +8,10 @@ after = os.popen('ls /dev/tty*').read()
 
 d = Differ()
 diff = d.compare(before, after)
-print(diff)
+plusdiff = "".join(diff)
+plus_start_index = plusdiff.find('+')
+plus_end_index = plusdiff.rfind('+') + 2
+new_port = plusdiff[plus_start_index:plus_end_index].strip('+').strip(' ')
+
+print(new_port)
+file.close()
