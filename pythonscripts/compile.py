8be2e3fdc5ad207fa823a777621acb1f513b1efe
kollstrom <e.kollstrom@gmail.com>
2016-04-13 23:43:35 +0200

    Compile right paths

--- pythonscripts/compile.py
+++ pythonscripts/compile.py
@@ -1,5 +1,5 @@
 import os
 
 os.system('export CLASSPATH="/home/pi/PiCode/jars/json-simple-1.1.1.jar:/home/pi/PiCode/src/"')
-os.system('javac c3pio/*.java')
-os.system('java /home/pi/PiCode/src/c3pio/Controller')
+os.system('javac -d /home/pi/PiCode/src/c3pio/*.java')
+os.system('java -cp /home/pi/PiCode/src/c3pio/ Controller')
