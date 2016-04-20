59a7892a29a2d89e60450fe7dffcfe1d10cee95b
kollstrom <e.kollstrom@gmail.com>
2016-04-20 15:44:03 +0200

    Fjerne ubrukte klasser, endret på python-skriptene og plassering av de

--- src/c3pio/arduino.py
+++ src/c3pio/arduino.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 import serial
-
+#  Opens and reads the port-number to the Arduino
 file = open('port.txt', 'r')
 port = file.read().strip('\n')  # Usikker på om .strip('\n') må være med
 file.close()
 
+#  Reads sensor-values that the Arduino is sending
 ser = serial.Serial(port, 9600)
 while 1:
     print(ser.readline())
